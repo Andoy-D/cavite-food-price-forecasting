c594e2ceb555b0893f66ffe78a07aae2b559aa19
chicharonega <erizmelvinbrioso08@gmail.com>
2026-06-10 22:11:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -71,8 +71,14 @@ html, body, [class*="css"] {
     background-color: #F7F8FC;
 }
 
-#MainMenu {display:none;}
+/* #MainMenu {display:none;} */
 footer {display:none;}
+
+[data-testid="collapsedControl"] {
+    display: flex !important;
+    visibility: visible !important;
+    opacity: 1 !important;
+}
 
 /* ===== SIDEBAR ===== */
 
